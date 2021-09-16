2294daa42688ad5c65474fc387404e953d74ae01
Aleksei Zhuravlev <dev.zhuravlev@gmail.com>
2021-09-16 20:14:13 +0300

    quickSort example

---
+++ algorithms/quick_sort.js
@@ -0,0 +1,37 @@
+
+
+const arr = [312,32,35,2,132,3,4346,568,5,2,1];
+
+function quick_sort(arr = [], leftBorder, rightBorder) {
+    if (!arr || arr.length < 2) return arr;
+
+    let pivot = arr[Math.floor((leftBorder + rightBorder) / 2) ];
+    let leftMarker = leftBorder;
+    let rightMarker = rightBorder;
+
+    while (leftMarker <= rightMarker) {
+
+        while(arr[leftMarker] < pivot) {
+            leftMarker++;
+        }
+
+        while (arr[rightMarker] > pivot) {
+            rightMarker--;
+        }
+
+        if(rightMarker >= leftMarker) {
+            let temp = arr[rightMarker];
+            arr[rightMarker] = arr[leftMarker];
+            arr[leftMarker] = temp;
+            leftMarker++;
+            rightMarker--;
+        }
+
+        if (leftBorder < rightMarker) { quick_sort(arr, leftBorder, rightMarker); }
+        if (leftMarker < rightBorder) { quick_sort(arr, leftMarker, rightBorder); }
+    }
+
+}
+
+quick_sort(arr, 0, arr.length - 1);
+console.log(arr);

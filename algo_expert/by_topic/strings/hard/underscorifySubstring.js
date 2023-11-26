b097b3938bdbbfead7f630d200946504299dfe46
ileksei <dev.zhuravlev@gmail.com>
2023-11-26 19:51:21 +0300

    added solutions

--- algo_expert/by_topic/strings/hard/underscorifySubstring.js
+++ algo_expert/by_topic/strings/hard/underscorifySubstring.js
@@ -1,23 +1,87 @@
+/**
+ *     Write a function that takes in two strings: a main string and a potential
+ *     substring of the main string. The function should return a version of the main
+ *     string with every instance of the substring in it wrapped between underscores.
+ *
+ *     If two or more instances of the substring in the main string overlap each
+ *     other or sit side by side, the underscores relevant to these substrings should
+ *     only appear on the far left of the leftmost substring and on the far right of
+ *     the rightmost substring. If the main string doesn't contain the other string
+ *     at all, the function should return the main string intact.
+ *
+ * Sample Input
+ * string = "qestthis is a testtesq to see if testestest it works"
+ * substring = "test"
+ *
+ * Sample Output
+ * "_test_this is a _testtest_ to see if _testestest_ it works"
+ */
 
-    
-//     Write a function that takes in two strings: a main string and a potential
-//     substring of the main string. The function should return a version of the main
-//     string with every instance of the substring in it wrapped between underscores.
-//
-//     If two or more instances of the substring in the main string overlap each
-//     other or sit side by side, the underscores relevant to these substrings should
-//     only appear on the far left of the leftmost substring and on the far right of
-//     the rightmost substring. If the main string doesn't contain the other string
-//     at all, the function should return the main string intact.
-//
-// Sample Input
-// string = "testthis is a testtest to see if testestest it works"
-// substring = "test"
-//
-// Sample Output
-// "_test_this is a _testtest_ to see if _testestest_ it works"
-
-
+/**
+ * time: N (N - length of string)
+ * space:
+ */
 function underscorifySubstring(string, substring) {
-    // Write your code here.
+    let entries = [];
+    let result = "";
+    let i = 0;
+    let j = i;
+
+    while (j < string.length) {
+        let currentSubstring = "";
+
+        // letter doesn't match
+        while (string[j] && string[j] !== substring[i]) {
+            currentSubstring += string[j];
+            j++;
+            // i++;
+        }
+
+        // push not pattern entries
+        if (currentSubstring) {
+            entries.push(currentSubstring);
+            currentSubstring = "";
+            i = 0;
+        }
+
+        // take substring entry
+        while (string[j] && string[j] === substring[i]) {
+            currentSubstring += string[j];
+            j++;
+            i++
+        }
+
+        // push pattern entry
+        if (currentSubstring) {
+            entries.push(currentSubstring);
+            currentSubstring = "";
+            i = 0;
+        }
+    }
+
+    for (let idx = 0; idx < entries.length; idx++) {
+        if (entries[idx] === substring && entries[idx + 1] === substring) {
+            result += "_testtest_";
+            ++idx;
+            continue;
+        }
+
+        if (entries[idx] === substring && entries[idx + 2] === substring) {
+            result += "_test" + entries[idx + 1] + "test_";
+            idx += 2;
+            continue;
+        }
+
+        if (entries[idx] === "test") {
+            result += "_test_";
+            continue
+        }
+
+        result += entries[idx];
+    }
+
+    return result;
 }
+
+// "_test_this is aa _testtest_ to see if _testestest_ it works"
+console.log(underscorifySubstring("testthis is a testtest to see if testestest it works", "test"))
--- algo_expert/by_topic/strings/super_hard/longestBalancedSubstring.js
+++ algo_expert/by_topic/strings/super_hard/longestBalancedSubstring.js
@@ -1,21 +1,66 @@
 
-    
-//     Write a function that takes in a string made up of parentheses ((
-//     and )). The function should return an integer representing the
-//     length of the longest balanced substring with regards to parentheses.
-//
-//     A string is said to be balanced if it has as many opening parentheses as it
-//     has closing parentheses and if no parenthesis is unmatched. Note that an
-//     opening parenthesis can't match a closing parenthesis that comes before it,
-//     and similarly, a closing parenthesis can't match an opening parenthesis that
-//     comes after it.
-//
-// Sample Input
-// string = "(()))("
-//
-// Sample Output
-// 4 // The longest balanced substring is "(())".
+/**
+*     Write a function that takes in a string made up of parentheses ((
+*     and )). The function should return an integer representing the
+*     length of the longest balanced substring with regards to parentheses.
+*
+*     A string is said to be balanced if it has as many opening parentheses as it
+*     has closing parentheses and if no parenthesis is unmatched. Note that an
+*     opening parenthesis can't match a closing parenthesis that comes before it,
+*     and similarly, a closing parenthesis can't match an opening parenthesis that
+*     comes after it.
+*
+* Sample Input
+* string = "(()))("
+*
+* Sample Output
+* 4 * The longest balanced substring is "(())".
+*/
 
 function longestBalancedSubstring(string) {
-    // Write your code here.
+    let stack = [];
+    let i = 0;
+    let maxLength = 0;
+    let currentLength = 0;
+
+    while (i < string.length) {
+
+        if (!stack.length && string[i] === ")") {
+            i++;
+            maxLength = maxLength > currentLength ? maxLength : currentLength;
+            currentLength = 0;
+            continue;
+        }
+
+        if (stack.length && string[i] === ")" && string[i + 1] === "(") {
+            stack.pop();
+            currentLength++;
+            maxLength = maxLength > currentLength ? maxLength : currentLength;
+            currentLength = 0;
+            i++;
+            continue;
+        }
+
+        if (string[i] === "(") {
+            stack.push(i);
+            i++;
+            continue;
+        }
+
+        if (string[i] === ")") {
+            stack.pop();
+            currentLength++;
+            i++;
+        }
+
+    }
+
+    return maxLength > currentLength ? maxLength : currentLength;
 }
+
+console.log(longestBalancedSubstring("(()))()")) // 2 (())
+console.log(longestBalancedSubstring("(()))(()((()))")) // 3 (())
+console.log(longestBalancedSubstring(")(()))()")) // 2 (())
+console.log(longestBalancedSubstring(")(()))()(((())))((")) // 4 (((())))
+console.log(longestBalancedSubstring("((()((")) // 1 ()
+console.log(longestBalancedSubstring(")))(")) //
